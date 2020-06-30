a31b676580fdb2dcb768cd1114a034eab2262bdf
ZAPHAKIEL <45849471+SAGIRI-kawaii@users.noreply.github.com>
2020-06-30 15:02:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import pymysql
 from itertools import chain
 
 
-BotQQ = 762802224 # 字段 qq 的值
-HostQQ = 1900384123 #主人QQ
-authKey = '1234567890' # 字段 authKey 的值
+BotQQ =  # 字段 qq 的值
+HostQQ =  #主人QQ
+authKey = '' # 字段 authKey 的值
 mirai_api_http_locate = 'localhost:8080/' # httpapi所在主机的地址端口,如果 setting.yml 文件里字段 "enableWebsocket" 的值为 "true" 则需要将 "/" 换成 "/ws", 否则将接收不到消息.
 app = Mirai(f"mirai://{mirai_api_http_locate}?authKey={authKey}&qq={BotQQ}")
 
